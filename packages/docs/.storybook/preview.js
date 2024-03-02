82dee8b2f1eea61000cf23b8899b056095f1d284
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-03-02 16:35:40 -0300

    Tema dark no Storybook

--- packages/docs/.storybook/preview.js
+++ packages/docs/.storybook/preview.js
@@ -1,6 +1,13 @@
 /** @type { import('@storybook/react').Preview } */
+import {themes} from '@storybook/theming'
 const preview = {
   parameters: {
+    docs: {
+      theme: themes.dark, // The replacement theme to use
+    },
+    backgrounds: {
+      default: themes.dark
+    },
     actions: { argTypesRegex: "^on[A-Z].*" },
     controls: {
       matchers: {
