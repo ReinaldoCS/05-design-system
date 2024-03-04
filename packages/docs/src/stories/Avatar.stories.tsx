0f882e482f0d36012e3910f041dab829678480e1
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-03-04 08:31:36 -0300

    Componente: Avatar

---
+++ packages/docs/src/stories/Avatar.stories.tsx
@@ -0,0 +1,22 @@
+import { Avatar, AvatarProps } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<AvatarProps> = {
+  title: 'Data display/Avatar',
+  component: Avatar,
+  args: {
+    src: 'https://github.com/reinaldocs.png',
+    alt: 'Reinaldo Correia',
+  },
+}
+
+export default meta
+type Story = StoryObj<AvatarProps>
+
+export const Primary: Story = {}
+
+export const WithFallback: Story = {
+  args: {
+    src: undefined,
+  },
+}
