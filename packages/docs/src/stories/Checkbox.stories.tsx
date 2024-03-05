b9299c9158906b16d3304d6ab92f399cd4fa00b4
ReinaldoCS <santosdoscorreiareinaldo@gmail.com>
2024-03-04 22:05:03 -0300

    Componente: Checkbox

---
+++ packages/docs/src/stories/Checkbox.stories.tsx
@@ -0,0 +1,24 @@
+import { Box, Checkbox, CheckboxProps, Text } from '@ignite-ui/react'
+import type { Meta, StoryObj } from '@storybook/react'
+
+const meta: Meta<CheckboxProps> = {
+  title: 'Form/Checkbox',
+  component: Checkbox,
+  args: {},
+  decorators: [
+    (Story) => (
+      <Box
+        as="label"
+        css={{ display: 'flex', flexDirection: 'row', gap: '$2' }}
+      >
+        <Story />
+        <Text size="sm">Accept terms of use</Text>
+      </Box>
+    ),
+  ],
+}
+
+export default meta
+type Story = StoryObj<CheckboxProps>
+
+export const Primary: Story = {}
